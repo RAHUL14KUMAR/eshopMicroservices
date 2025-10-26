ae78cf18371ba85c6d2c1108c347d01cc76eb9ef
RAHUL14KUMAR <rahul.kumar.it25@heritageit.edu.in>
2025-10-26 18:21:44 +0530

    apne samjhne k liye

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -2,8 +2,11 @@
 
 namespace BuildingBlocks.CQRS;
 
+// Unit is a special type from MediatR that basically means “no return value” (like void).
 public interface ICommand : ICommand<Unit> { }
 
+
+// keyword out is used on the generic type parameter TResponse
 public interface ICommand<out TResponse> : IRequest<TResponse>
 {
 
