2b97d77b23fee74f49487fc56da3a5225dd8bd9b
RAHUL14KUMAR <rahul.kumar.it25@heritageit.edu.in>
2025-10-26 18:21:04 +0530

    delete basket api

---
+++ src/Services/Basket/BasketAPI/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -0,0 +1,20 @@
+namespace BasketAPI.Basket.DeleteBasket;
+
+public record DeleteBasketResponse(bool IsSuccess);
+public class DeleteBasketEndpoint:ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{username}",async(string username,ISender sender)=>
+        {
+            var result = await sender.Send(new DeleteBasketCommand(username));
+            var response=result.Adapt<DeleteBasketResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("Delete Basket")
+        .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete Basket")
+        .WithDescription("Delete Basket");
+    }
+}
