d24459743a7fcb5a79a34e3a4e45b17e00306a5e
RAHUL14KUMAR <rahul.kumar.it25@heritageit.edu.in>
2025-10-31 01:41:04 +0530

    onmodelcreate we define how table is create, what are the column, relationship between the column

---
+++ src/Services/Ordering/OrderingInfrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+// using OrderingApplication.Data;
+using OrderingDomain.Models;
+using System.Reflection;
+
+namespace OrderingInfrastructure.Data;
+public class ApplicationDbContext : DbContext
+// IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : base(options) { }
+
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        // it scan what all configurations are there in the assembly(in a entity or db table) and apply those configurations like constraints, keys, relationships,column etc.
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
